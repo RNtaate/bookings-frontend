a5be6c4033b01f13b6a0c9eeda3a2592cc85cb0c
RNtaate <royhyde14@gmail.com>
2021-06-21 20:37:12 +0300

    Create handleOnChange, handleSubmit functions, use useState hook, axios to implement registration of users

--- src/components/auth/Registration.js
+++ src/components/auth/Registration.js
@@ -1,12 +1,48 @@
-import React from 'react';
+import React, {useState} from 'react';
+import axios from 'axios';
 
 let Registration = () => {
+
+  let [userDetails, setUserDetails] = useState({
+    username: '',
+    password: '',
+    password_confirmation: '',   
+  })
+
+  let handleOnChange = (e) => {
+    setUserDetails({...userDetails, [e.target.name]: e.target.value});
+  }
+
+  let handleSubmit = (e) => {
+    e.preventDefault();
+
+    let {username, password, password_confirmation} = userDetails;
+
+    axios.post("http://localhost:3001/registrations",{
+      user: {
+        username: username,
+        password: password,
+        password_confirmation: password_confirmation
+      }
+    }, {withCredentials: true} )
+    .then( response => {
+      setUserDetails({
+        username: '',
+        password: '',
+        password_confirmation: '',   
+      });
+      console.log('Registration response', response);
+    }).catch( error => {
+      console.log("Error", error);
+    })
+  }
+
   return (
     <div>
-      <form>
-        <input type="text" name="username" placeholder="Username"/>
-        <input type="password" name="password" placeholder="Password"/>
-        <input type="password" name="password_confirmation" placeholder="Password Confirmation"/>
+      <form onSubmit={handleSubmit}>
+        <input type="text" name="username" placeholder="Username" value={userDetails.username} onChange={handleOnChange} required/>
+        <input type="password" name="password" placeholder="Password" value={userDetails.password} onChange={handleOnChange} required />
+        <input type="password" name="password_confirmation" placeholder="Password Confirmation" value={userDetails.password_confirmation} onChange={handleOnChange} required />
         <button type="submit">Sign Up</button>
       </form>
     </div>
