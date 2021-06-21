de98051cc2ae3b4a8d486cdecd1f2ab7ef916fd9
RNtaate <royhyde14@gmail.com>
2021-06-21 19:49:23 +0300

    Import Registration component in Home.js file

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,8 +1,14 @@
 import React from 'react';
+import Registration from './auth/Registration';
 
 let Home = () => {
   return (
-    <h1>This is the home component</h1>
+    <div className="home_div">
+      <h1>This is the home component</h1>
+      <div className="reg_form_div">
+        <Registration />
+      </div>
+    </div>
   )
 }
 
