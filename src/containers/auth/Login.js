a9351a35340bdfb672de823529e6337c6223e4a6
RNtaate <royhyde14@gmail.com>
2021-06-22 15:00:35 +0300

    Connect login component to redux store, implement loggin in functionality with api and update global state

--- src/containers/auth/Login.js
+++ src/containers/auth/Login.js
@@ -1,10 +1,16 @@
 import React, { useState } from 'react';
+import axios from 'axios';
+import { connect } from 'react-redux';
 
-let Login = () =>{
+import {addUser} from '../../actions/index';
+import { API_URL } from '../Helpers/HelperConstants';
+
+let Login = (props) =>{
 
   let [loginDetails, setLoginDetails] = useState({
     username: '',
-    password: ''
+    password: '',
+    errorMessage: ''
   })
 
   let handleOnchange = (e) => {
@@ -13,16 +19,43 @@ let Login = () =>{
 
   let handleSubmit = (e) => {
     e.preventDefault();
-    console.log(loginDetails);
-    setLoginDetails({
-      username: '',
-      password: ''
+
+    let {username, password} = loginDetails;
+
+    axios.post(`${API_URL}/sessions`, {
+      user: {
+        username: username,
+        password: password,
+      }
+    }, {withCredentials: true})
+    .then( response => {
+
+      props.setCurrentUser(response.data.user);
+      if (response.data.user) {
+
+        console.log(response);
+  
+        setLoginDetails({
+          username: '',
+          password: '',
+          errorMessage: ''
+        })
+      }
+      else {
+        console.log('Something went wrong dude!');
+        setLoginDetails({ ...loginDetails, errorMessage: 'Username or Password is wrong'})
+      }
+
+    }).catch(error => {
+      console.log('Something is wrong: ', error);
+      setLoginDetails({ ...loginDetails, errorMessage: 'Network Error!, Please try again later'})
     })
   }
 
   return (
     <div>
       <h2>Login</h2>
+      <p>{loginDetails.errorMessage}</p>
       <form onSubmit={handleSubmit}>
         <input type="text" name="username" placeholder="Username" value={loginDetails.username} onChange={handleOnchange} />
         <input type="password" name="password" placeholder="Password" value={loginDetails.password} onChange={handleOnchange} />
@@ -32,4 +65,13 @@ let Login = () =>{
   )
 }
 
-export default Login;
+
+let mapDispatchToProps = (dispatch) => {
+  return ({
+    setCurrentUser: (userObj) => {
+      dispatch(addUser(userObj))
+    }
+  })
+}
+
+export default connect(null, mapDispatchToProps)(Login);
