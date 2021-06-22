e0fdd0e20cab1cd8720e3a60e158000041757eea
RNtaate <royhyde14@gmail.com>
2021-06-22 13:36:02 +0300

    Relocate component files to proper folder containers and components

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import Home from '../components/Home';
+import Home from '../containers/Home';
 
 function App() {
   return (
--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,5 +1,6 @@
 import React, {useState} from 'react';
 import Registration from './auth/Registration';
+import Login from './auth/Login';
 
 let Home = () => {
 
@@ -12,12 +13,17 @@ let Home = () => {
   return (
     <div className="home_div">
       <h1>This is the home component</h1>
+      <button>Login</button>
       <button onClick={toggleShowRegForm}>Sign up</button>
       <div>
         {
           showRegForm == true ? 
           <Registration showRegForm={showRegForm} toggleShowRegForm={toggleShowRegForm}/> : null
         }
+      </div>
+
+      <div>
+        <Login />
       </div>
     </div>
   )
