dfc2537882b362bbee04325602145c071d4e6152
RNtaate <royhyde14@gmail.com>
2021-06-22 12:47:04 +0300

    Update Home component to handle refreshing of registration form upon hiding

--- src/components/Home.js
+++ src/components/Home.js
@@ -13,7 +13,12 @@ let Home = () => {
     <div className="home_div">
       <h1>This is the home component</h1>
       <button onClick={toggleShowRegForm}>Sign up</button>
-      <Registration showRegForm={showRegForm} toggleShowRegForm={toggleShowRegForm}/>
+      <div>
+        {
+          showRegForm == true ? 
+          <Registration showRegForm={showRegForm} toggleShowRegForm={toggleShowRegForm}/> : null
+        }
+      </div>
     </div>
   )
 }
