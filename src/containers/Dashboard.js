ae142f301382a5fe05fd1666f8d4c2292a453c7c
RNtaate <royhyde14@gmail.com>
2021-06-23 15:27:38 +0300

    Implement checking of logged_in status befor rendering dashboard component, implement logging out

--- src/containers/Dashboard.js
+++ src/containers/Dashboard.js
@@ -1,9 +1,63 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useEffect } from 'react';
 import { connect } from 'react-redux';
 
+import {API_URL} from './Helpers/HelperConstants';
+import {addUser, removeUser} from '../actions/index';
+
 let Dashboard = (props) => {
+
+  let {myUserObj, setCurrentUser, logoutCurrentUser} = props;
+
+  let redirectToHome = () => {
+    props.history.push("/home");
+  }
+
+  let handleLogout = () => {
+    console.log('You clicked the logout button');
+    axios.delete(`${API_URL}/logout`, {withCredentials: true})
+    .then(response => {
+      console.log("I want to log out", response);
+      logoutCurrentUser();
+      redirectToHome();
+    }).catch( error => {
+      console.log("Sorry, couldn't logout smoothly, please try again", error);
+    })
+  }
+
+  useEffect(() => {
+    if (myUserObj.loggedInStatus === "NOT LOGGED IN") {
+      axios.get(`${API_URL}/logged_in`, {withCredentials: true})
+      .then( response => {
+        console.log("From the Dashboard: ",response)
+        if(response.data.logged_in) {
+          setCurrentUser(response.data.user);
+        }
+        else {
+          console.log('You are not logged in!, so I am taking you to the login/sign up page.');
+          redirectToHome();
+        }
+      }).catch( error => {
+        console.log('Something went wrong', error)
+        console.log('So I am kindly going to redirect you to the home page.')
+        redirectToHome();
+      })
+    }
+  }, []);
+
   return (
-    <h2>Welcome {props.myUserObj.user.username}</h2>
+    <div>
+      {
+        myUserObj.user ?(
+          <div>
+            <h2>Welcome {myUserObj.user.username}!</h2>
+            <button onClick={handleLogout}>Logout</button>
+          </div>
+        )
+        : null
+      }
+      <p>Dashboard for Norp Massage Parlor</p>
+    </div>
   )
 }
 
@@ -13,4 +67,15 @@ let mapStateToProps = (state) => {
   })
 }
 
-export default connect(mapStateToProps, null)(Dashboard);
+let mapDispatchToProps = (dispatch) => {
+  return ({
+    setCurrentUser: (userObj) => {
+      dispatch(addUser(userObj));
+    },
+    logoutCurrentUser: () => {
+      dispatch(removeUser());
+    }
+  })
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Dashboard);
