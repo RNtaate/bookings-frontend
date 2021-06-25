8404a787a04e65fd8896a509eba9b6db187529b7
RNtaate <royhyde14@gmail.com>
2021-06-26 01:34:06 +0300

    Create MassageDetails page, add it's routing to App.js component

--- src/components/App.js
+++ src/components/App.js
@@ -5,6 +5,7 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Home from '../containers/Home';
 import Dashboard from '../containers/Dashboard';
 import Appointments from '../containers/Appointments';
+import MassageDetails from '../containers/MassageDetails';
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
         <Route exact path="/" component={Dashboard}/>
         <Route exact path="/home" component={Home}/>
         <Route exact path="/appointments" component={Appointments} />
+        <Route path="/massage" component={MassageDetails} />
       </Switch>
     </BrowserRouter>
 
---
+++ src/containers/MassageDetails.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+let MassageDetails = () => {
+  return (
+    <div>
+      <h5>This is going to be the massage details page</h5>
+    </div>
+  )
+}
+
+export default MassageDetails
