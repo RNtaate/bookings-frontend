0e357a737e0271642946cf4759f889a514d6edea
RNtaate <royhyde14@gmail.com>
2021-06-25 15:59:25 +0300

    Create appointments list page

---
+++ src/containers/Appointments.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+let Appointments = () => {
+  return (
+    <div>
+      <p>This is going to be the appointments page</p>
+    </div>
+  )
+}
+
+export default Appointments;
