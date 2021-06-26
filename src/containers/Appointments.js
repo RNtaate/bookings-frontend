56ada7957e3d362d9f3b6565f6070f2c875b4fb5
RNtaate <royhyde14@gmail.com>
2021-06-26 05:11:14 +0300

    Connect Appointments component to redux store, creeate fetchAppointmentsList function

--- src/containers/Appointments.js
+++ src/containers/Appointments.js
@@ -1,6 +1,28 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { connect } from 'react-redux';
+import axios from 'axios';
+import {API_URL} from './Helpers/HelperConstants';
 
 let Appointments = () => {
+
+  let [localAppointments, setLocalAppointmnets] = useState({
+    appsList: [],
+    errorMessage: ''
+  })
+
+  let fetchAppointmentsList = () => {
+    axios.get(`${API_URL}/appointments`, {withCredentials: true})
+    .then( res => {
+      console.log(res);
+    }).catch (e => {
+      console.log('Something is wrong buddy', e);
+    })
+  }
+
+  useEffect(() => {
+    fetchAppointmentsList();
+  }, []);
+
   return (
     <div>
       <p>This is going to be the appointments page</p>
@@ -8,4 +30,10 @@ let Appointments = () => {
   )
 }
 
-export default Appointments;
+let mapStateToProps = (state) => {
+  return ({
+    myUserObj: state.userReducer
+  })
+}
+
+export default connect(mapStateToProps, null)(Appointments);
