5699893a510e54ac7c194fde2e56d833c3141e17
RNtaate <royhyde14@gmail.com>
2021-06-27 15:29:02 +0300

    Implement submission of appointment data to api, update local state, implement redirecting

--- src/containers/ModelForms/AppointmentForm.js
+++ src/containers/ModelForms/AppointmentForm.js
@@ -1,8 +1,10 @@
 import React, { useEffect, useState } from 'react'
 import { connect } from 'react-redux';
+import axios from 'axios';
 
 import { createDateToday, fetchLoggedInStatus } from '../Helpers/HelperMethods';
 import { addUser } from '../../actions/index';
+import { API_URL } from '../Helpers/HelperConstants';
 
 let AppointmentForm = (props) => {
 
@@ -13,9 +15,12 @@ let AppointmentForm = (props) => {
   let [localApt, setLocalApt] = useState({
     customer_name: "",
     massage_type: massage.name,
+    massage_id: massage.id,
     date: '',
     city: 'Kampala',
   })
+
+  let [errorMessage, setErrorMessage] = useState('');
 
   let handleOnChange = (e) => {
     setLocalApt({...localApt, customer_name: myUserObj.user.username, [e.target.name]: e.target.value});
@@ -24,6 +29,24 @@ let AppointmentForm = (props) => {
   let handleSubmit = (e) => {
     e.preventDefault();
     console.log(localApt);
+
+    let {customer_name, massage_type, massage_id, date, city} = localApt;
+    axios.post(`${API_URL}/appointments`,{
+      customer_name, 
+      massage_type, 
+      massage_id, 
+      date, 
+      city
+    }, {withCredentials:true})
+    .then( response => {
+      console.log(response);
+      setErrorMessage('');
+      setLocalApt({ ...localApt, date: '', city: 'Kampala'});
+      props.history.push("/appointments");
+    }).catch( e => {
+      console.log("Something went wrong, Appointment submission failed", e);
+      setErrorMessage("Oops!, Something went wrong, check your network and try again.")
+    })
   }
 
   useEffect( async () => {
@@ -38,9 +61,10 @@ let AppointmentForm = (props) => {
 
   return (
     <div>
+      <p>{errorMessage}</p>
       <form onSubmit={handleSubmit}>
-        <input type="date" name="date" placeholder="Preferred Date" onChange={handleOnChange} min={createDateToday()} required />
-        <select name="city" onChange={handleOnChange}>
+        <input type="date" name="date" placeholder="Preferred Date" onChange={handleOnChange} min={createDateToday()} required value={localApt.date} />
+        <select name="city" onChange={handleOnChange} value={localApt.city} >
           {cities.map( city => <option key={cities.indexOf(city)}>{city}</option>)}
         </select>
         <button type="submit">Create Appointment</button>
