fb593606412d788ac47e55de05f71a065fe487c1
RNtaate <royhyde14@gmail.com>
2021-06-27 15:29:37 +0300

    Implement fetching and displaying user appointmnets of the page

--- src/containers/Appointments.js
+++ src/containers/Appointments.js
@@ -7,15 +7,22 @@ let Appointments = () => {
 
   let [localAppointments, setLocalAppointmnets] = useState({
     appsList: [],
-    errorMessage: ''
+    errorMessage: 'Fetching appointments ...'
   })
 
   let fetchAppointmentsList = () => {
     axios.get(`${API_URL}/appointments`, {withCredentials: true})
     .then( res => {
       console.log(res);
+      if(res.data.length > 0) {
+        setLocalAppointmnets({ ...localAppointments, appsList: [ ...res.data], errorMessage: 'I have been replaced'})
+      }
+      else {
+        setLocalAppointmnets({ ...localAppointments, errorMessage: 'Sorry Buddy! You have no booked appointments'})
+      }
     }).catch (e => {
       console.log('Something is wrong buddy', e);
+      setLocalAppointmnets({ ...localAppointments, errorMessage: 'Something went wrong. This might be due to Network error!'})
     })
   }
 
@@ -25,7 +32,33 @@ let Appointments = () => {
 
   return (
     <div>
-      <p>This is going to be the appointments page</p>
+      <h4>YOUR BOOKED APPOINTMENTS</h4>
+      {
+        localAppointments.appsList.length > 0 ?
+        <table>
+          <thead>
+            <tr>
+              <th>MASSAGE TYPE</th>
+              <th>CITY</th>
+              <th>DATE</th>
+              <th>CUSTOMER NAME</th>
+            </tr>
+          </thead>
+          <tbody>
+            {localAppointments.appsList.map( app => {
+              return (
+                <tr key={localAppointments.appsList.indexOf(app)}>
+                  <td>{app.massage_type}</td>
+                  <td>{app.city}</td>
+                  <td>{app.date}</td>
+                  <td>{app.customer_name}</td>
+                </tr>
+              )
+            })}
+          </tbody>
+      </table>
+        : <p>{localAppointments.errorMessage}</p>
+      }
     </div>
   )
 }
