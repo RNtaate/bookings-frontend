8db76deeb6165536822c9c1b2e082be2d04fb82a
RNtaate <royhyde14@gmail.com>
2021-07-01 20:29:57 +0300

    Creaqte MassageCard.module.css file, add styling to MassageCard component

--- src/components/MassageCard.js
+++ src/components/MassageCard.js
@@ -1,15 +1,21 @@
 import React from 'react'
 import { Link } from 'react-router-dom';
 
+import * as styles from './stylesheets/MassageCard.module.css';
+
 let  MassageCard = (props) => {
 
   let {massageObj: massage} = props;
 
   return (
-    <div>
+    <div className={styles.massageCard_div}>
       <Link to={`/massage/${massage.id}`}>
-        <h2>{massage.name}</h2>
-        <img src={massage.massage_image.url}/>
+        <div className={styles.massageCard_inner_div}>
+          {/* <img src={massage.massage_image.url}/> */}
+          <div className={styles.massageCard_poster_div} style={{backgroundImage: `url(${massage.massage_image.url})`}}>
+          </div>
+          <h2>{massage.name}</h2>
+        </div>
       </Link>
     </div>
   )
