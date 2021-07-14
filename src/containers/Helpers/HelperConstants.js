b4d1c57513b6ced7e34058c594a9c61e11f538c0
RNtaate <royhyde14@gmail.com>
2021-07-14 09:34:34 +0300

    Update package.json, HelperConstants.js files

--- src/containers/Helpers/HelperConstants.js
+++ src/containers/Helpers/HelperConstants.js
@@ -1,5 +1,3 @@
-// const API_URL = 'https://serene-depths-82382.herokuapp.com';
-
-const API_URL = 'http://localhost:3001';
+const API_URL = 'https://serene-depths-82382.herokuapp.com';
 
 export default API_URL;
